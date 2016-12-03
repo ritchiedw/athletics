f4fb4017270062f1771c77e87b78d3103c5840dd
Dave Ritchie <ritchiedw@hotmail.com>
2016-12-03 22:16:08 +0000

    setting up form for create athlete.  Incomplete!

--- resources/views/athletes/create.blade.php
+++ resources/views/athletes/create.blade.php
@@ -28,6 +28,141 @@
     {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
 </div>
 
+<div class="form-group">
+    {!! Form::label('gender', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('dob', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('address_1', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('address_2', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('address_3', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('address_4', 'Firstname:', ['class' => 'control-label']) !!}
+    {!! Form::text('firstname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('postcode', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('parent_1_firstname', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('parent_1_surname', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('parent_1_email_address', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('parent_2_firstname', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('parent_2_surname', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('parent_2_email_address', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('telephone_number_1', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('telephone_number_2', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('telephone_number_3', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('telephone_number_4', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('emergency_contact_firstname', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('emergency_contact_surname', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('emergency_contact_relationship', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('membership_type', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('fees_paid', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('photo_consent', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('medical_consent_1', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('medical_consent_2', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('signed', 'Surname:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('surname', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('medical_info', 'Medical Information:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('medical_info', null, ['class' => 'form-control']) !!}
+</div>
+
 {!! Form::submit('Create New Athlete', ['class' => 'btn btn-primary']) !!}
 
 {!! Form::close() !!}
